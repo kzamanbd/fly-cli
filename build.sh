895fc6ca5056f5072e8834f0fa06a96e7026e3ed
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-08-04 18:12:15 +0600

    fix: build.sh file for generate exec

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Build for Windows
 echo "Building for Windows..."
-GOOS=windows GOARCH=amd64 go build -o fly.exe
+GOOS=windows GOARCH=amd64 go build -o bin/fly.exe
 
 if [ $? -ne 0 ]; then
     echo "An error has occurred! Aborting the script execution for windows..."
@@ -11,7 +11,7 @@ fi
 
 # Build for macOS
 echo "Building for macOS..."
-GOOS=darwin GOARCH=amd64 go build -o fly-mac
+GOOS=darwin GOARCH=amd64 go build -o bin/fly-mac
 
 if [ $? -ne 0 ]; then
     echo "An error has occurred! Aborting the script execution for macOS..."
@@ -20,7 +20,7 @@ fi
 
 # Build for Linux
 echo "Building for Linux..."
-GOOS=linux GOARCH=amd64 go build -o fly-linux
+GOOS=linux GOARCH=amd64 go build -o bin/fly-linux
 
 if [ $? -ne 0 ]; then
     echo "An error has occurred! Aborting the script execution for linux..."
